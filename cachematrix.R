4f1701dfd729b3b97cdbe3f528636d5807e4d696
ericglm <77827495+ericglm@users.noreply.github.com>
2021-01-22 17:42:05 +0800

    This is my final output

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will save any inversion of a matrix
+## so that the computation done need not be repeated
+## when the same computation is inputted again.
 
-## Write a short comment describing this function
+## The main purpose of this function is to condition
+## and allow the matrix to be a valid input for
+## the second function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) m <<- solve
+  getSolve <- function() m
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## This function is the function that a
+## programmer will use to compute a matrix as well
+## as cache the resulting output for future use.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getSolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setSolve(m)
+  m    
 }
+
+## I advice my fellow student who will evaluate my work
+## to first run the code I submitted above and then
+## input this other code in order to test my work:
+## (remove the double hashes please)
+## mat <- matrix(sample(1:40, 9), nrow = 3, ncol = 3)
+## matinput <- makeCacheMatrix(mat)
+## cacheSolve(matinput)
+## cacheSolve(matinput)
+
+## The last command will show the stored answer
